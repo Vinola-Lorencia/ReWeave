846b41b17cd748544d50cc828969e7323e3d9b1e
avell-123 <150067051+Avelinawong@users.noreply.github.com>
2025-07-13 22:35:23 +0700

    Mengedit fitu change profile

--- app/src/main/java/com/example/reweave/ui/Homes/HomesFragment.java
+++ app/src/main/java/com/example/reweave/ui/Homes/HomesFragment.java
@@ -1,5 +1,7 @@
 package com.example.reweave.ui.Homes;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +10,10 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.reweave.Model.User;
 import com.example.reweave.R;
+
+import io.realm.Realm;
 
 public class HomesFragment extends Fragment {
     TextView edtNama;
@@ -18,9 +23,17 @@ public class HomesFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         edtNama = root.findViewById(R.id.edtNama);
 
-        // ambil data dari activity
-        String namaUser = getActivity().getIntent().getStringExtra("nama_user");
-        edtNama.setText(namaUser);
+        // Ambil email dari session
+        SharedPreferences preferences = requireContext().getSharedPreferences("user_session", Context.MODE_PRIVATE);
+        String email = preferences.getString("user_email", null);
+
+        if (email != null) {
+            Realm realm = Realm.getDefaultInstance();
+            User user = realm.where(User.class).equalTo("email", email).findFirst();
+            if (user != null) {
+                edtNama.setText(user.getName());
+            }
+        }
 
         return root;
     }
